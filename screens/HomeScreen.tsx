b33b3592ec0522a11950b0e69293c58e6ff798b8
kimeungang <dmsrkd5004@naver.com>
2025-07-26 02:23:56 +0900

    스크롤 기능 추가

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,21 @@
-import {View} from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import { Button, View } from 'react-native';
+import { RootStackParamList } from '../types/navigation';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-const HomeScreen = ()=>{
-    return (
-        <View>
-
-        </View>
-    );
-}
+const HomeScreen = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  return (
+    <View>
+      <Button
+        title="go Matching Screen"
+        onPress={() => {
+          navigation.navigate('Matching', { id: 1, name: '김은강' });
+        }}
+      />
+    </View>
+  );
+};
 
 export default HomeScreen;
